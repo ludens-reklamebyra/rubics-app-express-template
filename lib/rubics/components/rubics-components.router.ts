8a30a4d1c94209a2b8b6757870ecb317db02b767
EmilsWebbod <emil@webbod.no>
2022-09-16 10:02:46 +0200

    Add easy way to extends store for component

--- lib/rubics/components/rubics-components.router.ts
+++ lib/rubics/components/rubics-components.router.ts
@@ -1,16 +1,22 @@
-import express from 'express';
+import express, { Request } from 'express';
 import { readdirSync } from 'fs';
 import { resolve } from 'path';
 import { postComponentsRender } from './rubics-components.controllers.js';
 
+const rubicsComponentsRouter = express();
 const components = readdirSync(resolve('src', 'components'));
 
-const rubicsComponentsRouter = express();
+// Extends store for components
+const componentStores: {
+  [key: string]: (req: Request) => object;
+} = {
+  MyComponent: (_req: Request) => ({}),
+};
 
 components.forEach((component) => {
   rubicsComponentsRouter.post(
     `/${component.toLowerCase()}`,
-    postComponentsRender(component)
+    postComponentsRender(component, componentStores[component])
   );
 });
 
